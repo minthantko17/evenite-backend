50ea692804cb787f045a288de4ecdebf8a45845a
minthantko <minthantko101@gmail.com>
2026-04-21 22:28:17 +0700

    add bilingual-field.dto, agenda-item.dto and genereated event.dto

---
+++ src/event/dto/agenda-item.dto.ts
@@ -0,0 +1,6 @@
+import { BilingualField } from './bilingual-field.dto';
+
+export interface AgendaItem {
+  time: string;
+  activity: BilingualField;
+}
---
+++ src/event/dto/bilingual-field.dto.ts
@@ -0,0 +1,4 @@
+export interface BilingualField {
+  en: string;
+  th: string;
+}
