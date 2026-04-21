229ca6b606497524c8fa234545a5b474a0090320
minthantko <minthantko101@gmail.com>
2026-04-21 22:58:45 +0700

    make event fields nullable to save as draft, and add save-draft.dto

---
+++ src/event/dto/save-draft.dto.ts
@@ -0,0 +1,26 @@
+import { BilingualField } from './bilingual-field.dto';
+import { AgendaItem } from './agenda-item.dto';
+import { EventCategory } from '../constants/event-category.constant';
+
+export class SaveDraftDto {
+  title?: BilingualField;
+  description?: BilingualField;
+  category?: EventCategory[];
+  location?: BilingualField;
+  mapLink?: string;
+  isOnline?: boolean;
+  startAt?: Date;
+  endAt?: Date;
+  seatLimit?: number;
+  hasCatering?: boolean;
+  isCateringFree?: boolean;
+  cateringDescription?: BilingualField;
+  agenda?: AgendaItem[];
+  contactName?: string;
+  contactEmail?: string;
+  contactPhone?: string;
+  contactLineId?: string;
+  externalRegistrationUrl?: string;
+  remarks?: BilingualField;
+  bannerUrl?: string;
+}
