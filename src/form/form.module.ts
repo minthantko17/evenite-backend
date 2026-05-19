e39bc263d453d3049498a439a39fb468484b6070
minthantko <minthantko101@gmail.com>
2026-05-19 23:40:59 +0700

    update form module

--- src/form/form.module.ts
+++ src/form/form.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { FormService } from './form.service';
 import { FormController } from './form.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { FormValidationService } from './services/form-validation.service';
+import { FormCrudService } from './services/form-crud.service';
 
 @Module({
-  providers: [FormService],
-  controllers: [FormController]
+  imports: [PrismaModule],
+  controllers: [FormController],
+  providers: [
+    FormService,
+    FormValidationService,
+    FormCrudService,
+  ],
 })
 export class FormModule {}
