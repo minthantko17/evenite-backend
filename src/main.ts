523338761a82fbf9b6bdb5c89c3a5f3cf438b6cf
minthantko <minthantko101@gmail.com>
2026-05-22 20:34:02 +0700

    add cors policy for vite localhost port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe({ 
     transform: true,
     whitelist: true, 
