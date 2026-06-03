f315f62f11e697a48542632203428da0864e5da9
minthantko <minthantko101@gmail.com>
2026-06-03 23:32:00 +0700

    fix existing event crud tests to pass, need to update test cases

--- src/event/exceptions/event-not-found.exception.ts
+++ src/event/exceptions/event-not-found.exception.ts
@@ -2,6 +2,6 @@ import { NotFoundException } from '@nestjs/common';
 
 export class EventNotFoundException extends NotFoundException {
   constructor() {
-    super('Event not found');
+    super('Event not found.');
   }
 }
