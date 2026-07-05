012cbe93ce1d34ab139ba29a9f6abcb9eea9e400
minthantko <minthantko101@gmail.com>
2026-07-06 00:31:21 +0700

    create registration validation service

--- src/registration/registration.module.ts
+++ src/registration/registration.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { RegistrationValidationService } from './services/registration-validation.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [],
-  providers: [],
+  providers: [RegistrationValidationService],
   exports: [],
 })
 export class RegistrationModule {}
