2ba4f3b7b70fab87fc329b79a0f1482d848e8c28
minthantko <minthantko101@gmail.com>
2026-07-08 04:33:10 +0700

    call user registered event and tickets from user module

--- src/registration/registration.controller.ts
+++ src/registration/registration.controller.ts
@@ -5,20 +5,16 @@ import {
   Delete,
   Param,
   Body,
-  Query,
   ParseUUIDPipe,
-  ParseEnumPipe,
   UseGuards,
   Req,
 } from '@nestjs/common';
-import { EventStatus, Role, TicketStatus } from '@prisma/client';
+import { Role } from '@prisma/client';
 import type { Request } from 'express';
 import { RegistrationService } from './registration.service';
 import { CreateRegistrationDto } from './dto/create-registration.dto';
 import { ReturnTicketDetailDto } from './dto/return-ticket-detail.dto';
-import { ReturnRegisteredEventDto } from './dto/return-registered-event.dto';
 import { ReturnRegistrantDto } from './dto/return-registrant.dto';
-import { ReturnParticipantTicketListDto } from './dto/return-participant-ticket-list.dto';
 import { JwtAccessGuard } from '../auth/guards/jwt-access.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../auth/decorators/roles.decorator';
@@ -79,47 +75,6 @@ export class RegistrationController {
     const user = req.user as JwtAccessPayload;
     return this.registrationService.getTicketByEventIdAndParticipantId(
       eventId,
-      user.participantProfileId!,
-    );
-  }
-
-  @Get('users/me/registered-events')
-  @Roles(Role.PARTICIPANT)
-  async getRegisteredEvents(
-    @Query('status', new ParseEnumPipe(EventStatus, { optional: true }))
-    status: EventStatus | undefined,
-    @Req() req: Request,
-  ): Promise<ReturnRegisteredEventDto[]> {
-    const user = req.user as JwtAccessPayload;
-    return this.registrationService.getRegisteredEvents(
-      user.participantProfileId!,
-      status,
-    );
-  }
-
-  @Get('users/me/tickets')
-  @Roles(Role.PARTICIPANT)
-  async getTickets(
-    @Query('ticketStatus', new ParseEnumPipe(TicketStatus, { optional: true }))
-    ticketStatus: TicketStatus | undefined,
-    @Req() req: Request,
-  ): Promise<ReturnParticipantTicketListDto[]> {
-    const user = req.user as JwtAccessPayload;
-    return this.registrationService.getTickets(
-      user.participantProfileId!,
-      ticketStatus,
-    );
-  }
-
-  @Get('users/me/tickets/:ticketId')
-  @Roles(Role.PARTICIPANT)
-  async getTicketById(
-    @Param('ticketId', ParseUUIDPipe) ticketId: string,
-    @Req() req: Request,
-  ): Promise<ReturnTicketDetailDto> {
-    const user = req.user as JwtAccessPayload;
-    return this.registrationService.getTicketById(
-      ticketId,
       user.participantProfileId!,
     );
   }
